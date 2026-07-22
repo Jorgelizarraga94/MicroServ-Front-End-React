8fafe24e109132610fedc5c319dd255c2f320cbd
Jorgelizarraga94 <jorgelizarraga1994@gmail.com>
2026-07-21 22:00:49 -0300

    evito la advertencia de ngrok en el navegador

--- src/api/apiClient.js
+++ src/api/apiClient.js
@@ -1,7 +1,11 @@
 import axios from 'axios';
 
 const apiClient = axios.create({
-    baseURL: import.meta.env.VITE_API_URL, // API Gateway antes localhost 8080
+    baseURL: import.meta.env.VITE_API_URL,
+    headers: {
+    'Content-Type': 'application/json',
+    'ngrok-skip-browser-warning': 'true', // <-- Evita la advertencia de ngrok en el navegador
+  }, // API Gateway antes localhost 8080
 });
 
 export default apiClient;
